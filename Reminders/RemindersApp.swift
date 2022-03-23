65c58fd606df93284a42a4068eab429eb5db4731
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-03-23 14:12:44 -0400

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Kevin Stradtman on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
